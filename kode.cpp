5b251f7ff46bc6b2dd0138e97806407e35c2dd9b
Ghesvy1105 <epialfitsa@gmail.com>
2023-12-06 23:46:58 +0700

    tampilkandata

--- kode.cpp
+++ kode.cpp
@@ -18,3 +18,22 @@ struct Karyawan {
 const int max_karyawan = 2;
 Karyawan dataKaryawan[max_karyawan];
 int jumlahKaryawan = 0;
+
+void tampilkanDataKaryawan() {
+    cout << "							\n"; 
+    cout << "===== Data Karyawan =====\n";
+    for (int i = 0; i < jumlahKaryawan; ++i) {
+        cout << "Nama: " << dataKaryawan[i].nama << endl;
+        cout << "NIK: " << dataKaryawan[i].nik << endl;
+        cout << "Tanggal Lahir: " << dataKaryawan[i].tanggalLahir << endl;
+        cout << "Status: " << dataKaryawan[i].status << endl;
+        cout << "Jabatan: " << dataKaryawan[i].jabatan << endl;
+        cout << "Level: " << dataKaryawan[i].level << endl;
+        cout << "Divisi: " << dataKaryawan[i].divisi << endl;
+        cout << "Gaji: " << dataKaryawan[i].gaji << endl;
+        cout << "Masa Kerja: " << dataKaryawan[i].masaKerja << " tahun\n";
+        cout << "Jenis Kelamin: " << (dataKaryawan[i].jenisKelamin == 'L' ? "Laki-laki" : "Perempuan") << endl;
+        cout << "------------------------\n";
+    }
+}
+
